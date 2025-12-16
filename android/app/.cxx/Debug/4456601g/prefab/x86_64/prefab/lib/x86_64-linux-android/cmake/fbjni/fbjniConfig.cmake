90adcc089b3526fed4266b7b6c944125836ba6af
Maroof-Ramay <marooframay@gmail.com>
2025-12-16 21:40:57 +0500

    Ignore android/app/.cxx/Debug folder

--- android/app/.cxx/Debug/4456601g/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "/Users/ninjaassassin/.gradle/caches/9.0.0/transforms/3adb6efc9a2445a38733a5eba88c31ec/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86_64/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/ninjaassassin/.gradle/caches/9.0.0/transforms/3adb6efc9a2445a38733a5eba88c31ec/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
